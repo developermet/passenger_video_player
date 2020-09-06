54e5499b4ad48a71dc5f1f6531f5ba5e7be49cc0
developermet <oe.anderson@cerezostudio.com>
2020-09-06 00:25:37 -0500

    Adding randm movie to index page - fixing streaming

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'), fs = require('fs'), router = express.Router(), path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SITP - Transmilenio'});
+  const directoryPath = "public/media";
+	fs.readdir(directoryPath, function (err, files) {
+    if (err) return console.log('Unable to scan directory: ' + err); 
+		else {
+      var name = "", randomFile = files[Math.floor(Math.random() * files.length)], data = '';
+      tmp = fs.readdirSync(directoryPath+"/"+randomFile);
+      tmp.forEach(function(archive) {
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".mp4") name = archive;
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".jpg") movieImage = archive;
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".txt") data = fs.readFileSync(directoryPath + "/" + randomFile + "/" + archive, 'utf-8');
+      });
+      res.render('index', { title: 'SITP - Transmilenio', directoryPath: directoryPath, folderName: randomFile, movieImage: movieImage, synopsis: data, file: name});
+		}
+	});
 });
 
 module.exports = router;
--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,4 @@
-var express = require('express');
-const fs = require('fs');
-const path = require('path');
-var router = express.Router();
+const express = require('express'), fs = require('fs'), path = require('path'), router = express.Router();
 
 router.get('/', function (req, res) {
 	const directoryPath = "public/media";
@@ -12,10 +9,10 @@ router.get('/', function (req, res) {
 			files.forEach(function(file) {
 				tmp = fs.readdirSync(directoryPath+"/"+file);
 				tmp.forEach(function(archive) {
-					if (path.extname(directoryPath + "/" + archive) == ".jpg") results[file] = archive;	
+					if (path.extname(directoryPath + "/" + archive) == ".jpg") results[file] = archive;
 				});
 			});
-			res.render('video', { title: 'SITP - Transmilenio', videos: (results.length > 0) ? results : [], directoryPath: directoryPath});
+			res.render('video', { title: 'SITP - Transmilenio', videos: results, directoryPath: directoryPath});
 		}
 	});
 });
@@ -25,20 +22,21 @@ router.get('/:folderName', function(req, res) {
 	fs.readdir(directoryPath, function (err, files) {
     if (err) return console.log('Unable to scan directory: ' + err); 
 		else {
-			var name = ""
+			var name = "", data = "";
 			files.forEach(function(file) {
 				if (path.extname(directoryPath + "/" + file) == ".mp4") name = file;
+				if (path.extname(directoryPath + "/" + file) == ".txt") data = fs.readFileSync(directoryPath + "/" + file, 'utf-8');
 			});
-			res.render('selectedVid', { title: 'SITP - Transmilenio', folderName: req.params.folderName, file: name});
+			res.render('selectedVid', { title: 'SITP - Transmilenio', folderName: req.params.folderName, synopsis: data, file: name});
 		}
 	});
 });
 
 /* Play a video */
 router.get('/:folderName/:videoName', function(req, res) {
-  const path = "public/media/"  + req.params.folderName + "/" + req.params.videoName , stat = fs.statSync(path), fileSize = stat.size, range = req.headers.range;
+	const location = "public/media/"  + req.params.folderName + "/" + req.params.videoName, stat = fs.statSync(location), fileSize = stat.size, range = req.headers.range;
 	if (range) {
-    const parts = range.replace(/bytes=/, "").split("-"), start = parseInt(parts[0], 10), end = parts[1] ? parseInt(parts[1], 10) : fileSize-1, chunksize = (end-start)+1, file = fs.createReadStream(path, {start, end}), 
+    const parts = range.replace(/bytes=/, "").split("-"), start = parseInt(parts[0], 10), end = parts[1] ? parseInt(parts[1], 10) : fileSize-1, chunksize = (end-start)+1, file = fs.createReadStream(location, {start, end}), 
     head = {
 			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
 			'Accept-Ranges': 'bytes',
@@ -55,6 +53,7 @@ router.get('/:folderName/:videoName', function(req, res) {
 		res.writeHead(200, head);
 		fs.createReadStream(path).pipe(res);
 	}
+	//console.log(path.resolve(location)); 
 });
 
 module.exports = router;
