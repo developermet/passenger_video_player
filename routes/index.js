f131260d0af9cce7d79f0dd5b42da3a098f4a9f3
developermet <oe.anderson@cerezostudio.com>
2020-10-02 07:19:01 -0500

    Fixing minor details

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const express = require('express'), fs = require('fs'), router = express.Router(
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SITP - Transmilenio', navbar: true});
+  const directoryPath = "public/media/videos";
+	fs.readdir(directoryPath, function (err, files) {
+    if (err) return console.log('Unable to scan directory: ' + err); 
+		else {
+      var name = "", randomFile = files[Math.floor(Math.random() * files.length)], data = '';
+      tmp = fs.readdirSync(directoryPath+"/"+randomFile);
+      tmp.forEach(function(archive) {
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".mp4") name = archive;
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".jpg") movieImage = archive;
+        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".txt") data = fs.readFileSync(directoryPath + "/" + randomFile + "/" + archive, 'utf-8');
+      });
+      res.render('index', { title: 'SITP - Transmilenio', directoryPath: directoryPath, folderName: randomFile, movieImage: movieImage, synopsis: data, file: name});
+		}
+	});
 });
 
 module.exports = router;
