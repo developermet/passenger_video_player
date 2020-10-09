4b181d547b685dbba7ca1a53a1daeb9d6875fddc
developermet <oe.anderson@cerezostudio.com>
2020-10-09 09:39:25 -0500

    Fixing navbar

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 const express = require('express'), fs = require('fs'), router = express.Router(), path = require('path');
 
 
+
+router.get('/login', function(req, res) {
+  res.render('initialForm', { navbar: 1 });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const directoryPath = "public/media/videos";
-	fs.readdir(directoryPath, function (err, files) {
-    if (err) return console.log('Unable to scan directory: ' + err); 
-		else {
-      var name = "", randomFile = files[Math.floor(Math.random() * files.length)], data = '';
-      tmp = fs.readdirSync(directoryPath+"/"+randomFile);
-      tmp.forEach(function(archive) {
-        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".mp4") name = archive;
-        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".jpg") movieImage = archive;
-        if (path.extname(directoryPath + "/" + randomFile + "/" + archive) == ".txt") data = fs.readFileSync(directoryPath + "/" + randomFile + "/" + archive, 'utf-8');
-      });
-      res.render('index', { title: 'SITP - Transmilenio', directoryPath: directoryPath, folderName: randomFile, movieImage: movieImage, synopsis: data, file: name});
-		}
-	});
+  res.render('index', { navbar: 0 });
+});
+
+router.get('/terms', function(req, res) {
+  res.render('terms', { navbar: 2 });
 });
 
 module.exports = router;
