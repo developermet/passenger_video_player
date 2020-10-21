efd0f519f41d9a6f5f75c4997fbe8c6066f8e5d9
developermet <oe.anderson@cerezostudio.com>
2020-10-20 22:04:52 -0500

    Adding first user model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  study: {
+    type: String,
+    default: true
+  },
+  occupation: {
+    type: String,
+    default: true
+  }, 
+  user_connectios: { 
+    type : Array, 
+    default: [] 
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
