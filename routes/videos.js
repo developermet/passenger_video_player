fef30619056757e5e010dae2d38695d97fae7f1c
developermet <oe.anderson@cerezostudio.com>
2020-10-30 09:36:37 -0500

    New video views show and play

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,6 @@
 const express = require('express'), fs = require('fs'), path = require('path'), router = express.Router(), { ensureAuthenticated } = require('../config/auth');
 
-router.get('/', ensureAuthenticated, function (req, res) {
+router.get('/', ensureAuthenticated, (req, res) => {
 	const directoryPath = "public/media/videos";
 	fs.readdir(directoryPath, function (err, files) {
     if (err) return console.log('Unable to scan directory: ' + err); 
@@ -17,7 +17,23 @@ router.get('/', ensureAuthenticated, function (req, res) {
 	});
 });
 
-router.get('/:folderName', ensureAuthenticated, function(req, res) {
+router.get('/selected/:folderName', ensureAuthenticated, (req, res) => {
+	const directoryPath = "public/media/videos/" + req.params.folderName;
+	fs.readdir(directoryPath, function (err, files) {
+    if (err) return console.log('Unable to scan directory: ' + err); 
+		else {
+			var thumb, data = "";
+			files.forEach(function(file) {
+				if (path.extname(directoryPath + "/" + file) == ".mp4") name = file;
+				if (path.extname(directoryPath + "/" + file) == ".jpg") thumb = file;
+				if (path.extname(directoryPath + "/" + file) == ".txt") data = fs.readFileSync(directoryPath + "/" + file, 'utf-8');
+			});
+			res.render('showVid', { title: 'SITP - Transmilenio', directoryPath: directoryPath, folderName: req.params.folderName, synopsis: data, thumbnail: thumb, navbar: 2 });
+		}
+	});
+});
+
+router.get('/play/:folderName', ensureAuthenticated, (req, res) => {
 	const directoryPath = "public/media/videos/" + req.params.folderName;
 	fs.readdir(directoryPath, function (err, files) {
     if (err) return console.log('Unable to scan directory: ' + err); 
@@ -25,15 +41,14 @@ router.get('/:folderName', ensureAuthenticated, function(req, res) {
 			var name = "", data = "";
 			files.forEach(function(file) {
 				if (path.extname(directoryPath + "/" + file) == ".mp4") name = file;
-				if (path.extname(directoryPath + "/" + file) == ".txt") data = fs.readFileSync(directoryPath + "/" + file, 'utf-8');
 			});
-			res.render('selectedVid', { title: 'SITP - Transmilenio', folderName: req.params.folderName, synopsis: data, file: name, navbar: 0});
+			res.render('selectedVid', { title: 'SITP - Transmilenio', directoryPath: directoryPath, folderName: req.params.folderName, file: name, navbar: 2});
 		}
 	});
 });
 
 /* Play a video */
-router.get('/:folderName/:videoName', function(req, res) {
+router.get('/:folderName/:videoName', (req, res) => {
 	const location = "public/media/videos/"  + req.params.folderName + "/" + req.params.videoName, stat = fs.statSync(location), fileSize = stat.size, range = req.headers.range;
 	if (range) {
     const parts = range.replace(/bytes=/, "").split("-"), start = parseInt(parts[0], 10), end = parts[1] ? parseInt(parts[1], 10) : fileSize-1, chunksize = (end-start)+1, file = fs.createReadStream(location, {start, end}), 
