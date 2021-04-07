3d91c3c3aca6d8cb2eaea09fd7b3b94fdc5919b1
Anderson <desarollador.ti1@metgroupsas.com>
2021-04-06 20:09:25 -0500

    wrong var use

--- routes/index.js
+++ routes/index.js
@@ -129,7 +129,7 @@ router.post('/tmsaroutedata', async (req, res) => {
 
 router.post('/connectedUsers', async (req, res) => {
   console.log(req.body);
-  let user = {traveler_kind: req.body.traveler_kind, stratum: req.body.stratum, age: req.body.age, gender: req.body.gender, busId: global,busId, routeId: global.routeId}
+  let user = {traveler_kind: req.body.traveler_kind, stratum: req.body.stratum, age: req.body.age, gender: req.body.gender, busId: global.busId, routeId: global.routeId}
   await tables.addUser(user).then(user => res.sendStatus(200)).catch(err => res.sendStatus(400));
 });
 
