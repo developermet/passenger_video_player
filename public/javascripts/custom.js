5887a2721d7914a71733b65e35ef02d8845d293d
JulianMGonzalez <monterogjulian@gmail.com>
2023-12-12 16:57:37 -0500

    fix bug in map rendering with its stops

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,13 +1,13 @@
-(function($) {
+(function ($) {
   "use strict";
 
-   $(document).on('click', '#sidebarToggle', function(e) {  
+  $(document).on('click', '#sidebarToggle', function (e) {
     e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
   });
 
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
     if ($window.width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
@@ -16,22 +16,22 @@
     }
   });
 
-  $('#toTop').on('click', function(e) {
+  $('#toTop').on('click', function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, 500);
     return false;
   })
 
-  $('.pop').on('click', function() {
+  $('.pop').on('click', function () {
     $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-    $('#imagemodal').modal('show');   
-  });		
-  
+    $('#imagemodal').modal('show');
+  });
+
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav({
     closeOnClick: true
   });
-  
+
   $('.slider').slider();
 
   $('.collapsible').collapsible();
@@ -41,9 +41,9 @@
   $('.modal').modal();
 
   $('select').formSelect();
-  
+
   removeContainer();
-  
+
 })(jQuery);
 
 let interval_id = null, map = null, big_interval_id = null, marker = null, mapFiles = {}, mapUpdater = null, iterator = 0, mapRender = true;
@@ -68,7 +68,7 @@ function changeTexts() {
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF'.split(''), color = '#';
-  for (var i = 0; i < 6; i++ ) {
+  for (var i = 0; i < 6; i++) {
     color += letters[Math.round(Math.random() * 15)];
   }
   return color;
@@ -85,27 +85,65 @@ function playVideo() {
 }
 
 function setMap() {
-  var crs = new L.Proj.CRS('EPSG:4686','+proj=longlat +units=m +no_defs', {origin: [-400.0, 399.9999999999998], resolutions: [0.0027496601869330985,0.001374830093467739,6.874150467326798E-4,3.437075233663399E-4,1.7185376168316996E-4,8.592688084158498E-5,4.296344042198222E-5,2.148172021099111E-5,1.0740860104305824E-5,5.3704300533426425E-6,2.685215025481591E-6,1.3426075127407955E-6]}); 
-  map = L.map('mapid',{crs: crs}), busIcon = L.icon({iconUrl: '/public/images/bus-marker.png', iconSize: [40, 40]}); 
-  marker = L.marker([4.486196, -74.107678], {icon: busIcon}), icon = L.icon({iconUrl: '/public/images/little-square.png'});
-  map.setView([4.486196, -74.107678], 9);
+  //la configuracion del CRS se puede llevar a otro archivo configurable para mejor uso de los estilos
+  var crs = new L.Proj.CRS('EPSG:4686', '+proj=longlat +units=m +no_defs', {
+    origin: [-400.0, 399.9999999999998],
+    resolutions: [
+      1.4078260157301528,
+      0.7039130078650764,
+      0.3519565039325382, 
+      0.1759782519662691, 
+      0.08798912597123724, 
+      0.043994562997515925, 
+      0.021997281496378505, 
+      0.010998640746999522, 
+      0.005499320373499761, 
+      0.0027496601879396106, 
+      0.001374830093493913, 
+      6.874150466279835E-4,
+      3.4370752343296484E-4,
+      1.718537616926878E-4,
+      8.592688083444659E-5,
+      4.2963440417223295E-5,
+      2.148172021099111E-5,
+      1.0740860104305824E-5,
+      5.3704300533426425E-6,
+      2.685215025481591E-6,
+      1.3426075127407955E-6,
+      6.713037563703978E-7,
+      3.356518781851989E-7,
+      1.6782593909259944E-7
+    ]
+  });
+
+  var map = L.map('mapid', { crs: crs }).setView([4.486196, -74.107678], 9);
+
+  const busIcon = L.icon({ iconUrl: '/public/images/bus-marker.png', iconSize: [40, 40] });
+  const marker = L.marker([4.486196, -74.107678], { icon: busIcon })
+
+  const markerIcon = L.icon({ iconUrl: '/public/images/little-square.png' });
+
   requestFull(map);
+
   L.esri.tiledMapLayer({
-    url: 'https://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/mapa_hibrido_4686/MapServer',
-    maxZoom:10,
-	  minZoom:5
+    url: 'https://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/mapa_base_4686/MapServer?f=json',
+    maxZoom: 22,
+    minZoom: 1
   }).addTo(map);
-  var stops = L.esri.featureLayer({
-    url: 'https://gis.transmilenio.gov.co/arcgis/rest/services/Zonal/consulta_paraderos_zonales/FeatureServer/1',
+
+  const stops = L.esri.featureLayer({
+    url: 'https://gis.transmilenio.gov.co/arcgis/rest/services/Zonal/consulta_paraderos_zonales/FeatureServer/0',
     pointToLayer: function (geojson, latlng) {
       return L.marker(latlng, {
-        icon: icon
+        icon: markerIcon
       });
     }
   }).addTo(map);
+  
   stops.bindPopup(function (layer) {
     return L.Util.template('<ul><li><b>Nombre:</b>{nombre_paradero}</li><li><b>Dirección: </b>{direccion_paradero}</li></ul>', layer.feature.properties);
   });
+
   marker.addTo(map);
 }
 
@@ -118,7 +156,7 @@ function updateMap(location) {
 
 async function removeContainer() {
   var pathname = window.location.pathname, container = null, video = null;
-  if (pathname.includes("/announcer")){
+  if (pathname.includes("/announcer")) {
     container = document.getElementById('main-container');
     video = document.getElementById('video-container-div');
     unwrap(container);
@@ -128,24 +166,24 @@ async function removeContainer() {
 }
 
 function unwrap(wrapper) {
-	var docFrag = document.createDocumentFragment(), child = undefined;
-	while (wrapper.firstChild) {
-		child = wrapper.removeChild(wrapper.firstChild);
-		docFrag.appendChild(child);
-	}
-	wrapper.parentNode.replaceChild(docFrag, wrapper);
+  var docFrag = document.createDocumentFragment(), child = undefined;
+  while (wrapper.firstChild) {
+    child = wrapper.removeChild(wrapper.firstChild);
+    docFrag.appendChild(child);
+  }
+  wrapper.parentNode.replaceChild(docFrag, wrapper);
 }
 
 function videoAndMap(files) {
   const pathname = window.location.pathname;
-  if (pathname.includes("/announcer")){
+  if (pathname.includes("/announcer")) {
     files = files.split(',');
     var howMany = files.length - 1, video = document.getElementById('video-player-annoucements'), source = document.querySelector("#video-player-annoucements > source"), mapDIV = document.getElementById('mapid'), newUrl = '';
-    iterator = (iterator < howMany) ? iterator+1 : 0;
+    iterator = (iterator < howMany) ? iterator + 1 : 0;
     newUrl = encodeURI(window.location.origin + "/video/adds/" + files[iterator])
-    if (iterator%2 == 0) {
+    if (iterator % 2 == 0) {
       video.style.display = 'none';
-      mapDIV.style.display = 'block';     
+      mapDIV.style.display = 'block';
       if (mapRender) {
         mapFiles = setMap();
         mapRender = false;
@@ -187,11 +225,11 @@ function animateScroll() {
 }
 
 function scroller(target, offset, times) {
-  target.scroll({top: offset*times, behavior: 'smooth'});
+  target.scroll({ top: offset * times, behavior: 'smooth' });
 }
 
 async function displayMessage(msg) {
-  const target = document.getElementById('message-display'), msgDIV = document.getElementById('information-target'), pathname = window.location.pathname; 
+  const target = document.getElementById('message-display'), msgDIV = document.getElementById('information-target'), pathname = window.location.pathname;
   if (pathname.includes("/announcer")) {
     msgDIV.innerHTML = msg;
     animateScroll();
