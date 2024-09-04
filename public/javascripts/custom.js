bb0fde62d25800bcf700e6bc6aca09b90ad14b58
JulianMGonzalez <monterogjulian@gmail.com>
2024-09-04 08:05:36 -0500

    fix bug 404 video

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -291,50 +291,56 @@ function unwrap(wrapper) {
 }
 
 function videoAndMap(files) {
-  const pathname = window.location.pathname;
-  if (pathname.includes("/announcer")) {
-    if (!files || files.trim() === "") {
-      // Si no hay archivos de video, mostrar el mapa directamente
-      mostrarMapa();
-      return;
-    }
-
-    files = files.split(",");
-    var howMany = files.length - 1,
-      video = document.getElementById("video-player-annoucements"),
-      source = document.querySelector("#video-player-annoucements > source"),
-      mapDIV = document.getElementById("mapid"),
-      newUrl = "";
-    iterator = iterator < howMany ? iterator + 1 : 0;
-    newUrl = encodeURI(
-      window.location.origin + "/video/adds/" + files[iterator]
-    );
-    if (iterator % 2 == 0) {
-      video.style.display = "none";
-      mapDIV.style.display = "block";
-      isSwitchMap = true
-      if (mapRender) {
-        mapFiles = setMap();
-        mapRender = false;
-      }
-      if(lastLocation){
-        updateMap(lastLocation)
+  try {
+    const pathname = window.location.pathname;
+    if (pathname.includes("/announcer")) {
+      console.log(files);
+      if (!files || files.trim() === "" || files === "null") {
+        // Si no hay archivos de video, mostrar el mapa directamente
+        mostrarMapa();
+        return;
       }
 
-      setTimeout(() => {
+      files = files.split(",");
+      var howMany = files.length - 1,
+        video = document.getElementById("video-player-annoucements"),
+        source = document.querySelector("#video-player-annoucements > source"),
+        mapDIV = document.getElementById("mapid"),
+        newUrl = "";
+      iterator = iterator < howMany ? iterator + 1 : 0;
+      newUrl = encodeURI(
+        window.location.origin + "/video/adds/" + files[iterator]
+      );
+      if (iterator % 2 == 0) {
+        video.style.display = "none";
+        mapDIV.style.display = "block";
+        isSwitchMap = true
+        if (mapRender) {
+          mapFiles = setMap();
+          mapRender = false;
+        }
+        if (lastLocation) {
+          updateMap(lastLocation)
+        }
+
+        setTimeout(() => {
+          source.src = newUrl;
+          video.load();
+          video.style.display = "block";
+          mapDIV.style.display = "none";
+          isSwitchMap = false;
+          video.play();
+        }, 30000);
+      } else {
         source.src = newUrl;
         video.load();
-        video.style.display = "block";
-        mapDIV.style.display = "none";
-        isSwitchMap = false;
         video.play();
-      }, 30000);
-    } else {
-      source.src = newUrl;
-      video.load();
-      video.play();
+      }
     }
+  } catch (error) {
+    mostrarMapa()
   }
+
 }
 
 function mostrarMapa() {
@@ -344,9 +350,7 @@ function mostrarMapa() {
   video.style.display = "none";
   mapDIV.style.display = "block";
   isSwitchMap = true;
-  console.log('Entra?');
   if (mapRender) {
-    console.log("mapa renderizado", mapRender);
     mapFiles = setMap();
     mapRender = false;
   }
@@ -417,7 +421,7 @@ socket.on("location", (data) => {
   //solo se actualiza el mapa si ya esta cargado, la variable map 
   if (!mapRender && isSwitchMap) updateMap(data);
 
-  if(!isSwitchMap) {
+  if (!isSwitchMap) {
     lastLocation = data
   }
 });
